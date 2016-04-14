8469d48a126f2180d1a58fc854c5df0c633ee8db
raman_zhuravski <raman_zhuravski@epam.com>
2016-04-14 12:03:57 +0300

    add crud operation

--- module6/src/main/java/com/romario/mentoring/webservice/processor/PersonProcessor.java
+++ module6/src/main/java/com/romario/mentoring/webservice/processor/PersonProcessor.java
@@ -15,4 +15,21 @@ public class PersonProcessor {
   public List<Person> getPersonsFromDepartment(int departmentId) {
     return new ArrayList<Person>();
   }
+
+  public Person createPerson(Person person) {
+
+    return new Person( 1, "test", 1 );
+  }
+
+  public Person updatePerson(final Person person) {
+    return new Person( 1, "test", 1 );
+  }
+
+  public Person removePerson(final Person person) {
+    return new Person( 1, "test", 1 );
+  }
+
+  public List<Person> getPersons() {
+    return new ArrayList<Person>(  );
+  }
 }
